7afa4df1cd39933c0e67452e46f84229fe85198d
Vince55 <vincent.nguyenv10@gmail.com>
2016-12-02 04:57:22 -0500

    Add files via upload

---
+++ table.py
@@ -0,0 +1,31 @@
+class Table():
+    ''' '''
+
+    def __init__(self):
+        '''
+        '''
+        self._data = {}
+
+    def set_column(self, column_name, data):
+        self._data[column_name] = data
+
+    def set_row(self, row_num, data):
+        '''(Table, int, str)
+        '''
+
+class Database():
+    ''' '''
+
+    def __init__():
+        '''
+        '''
+        self._data = {}
+
+    def add_table(self, table_name, table):
+        '''
+        '''
+        self._data[table_name] = table
+
+
+
+
