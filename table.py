b083a6a4fc9ac4ce28cd98ddd6cdfdf3b634f6da
Vince55 <vincent.nguyenv10@gmail.com>
2016-12-03 06:35:25 -0500

    Table and Databse ADTs
    Methods and descriptions of Table and Database classes

--- table.py
+++ table.py
@@ -17,24 +17,44 @@ class Table():
     def delete_column(self, column_name):
         '''
         '''
+        del self._data[column_name]
+
+    def get_headers(self):
+        '''
+        '''
+        header_list = list(self._data.keys())
+        return header_list
 
     def set_row(self, row_num, data):
         '''(Table, int, str) -> NoneType
         Adds a row to the table. Done by adding each element of the row into
         its respective columns.
         '''
+        pass
 
-    def delete_row(self, row_num):
+    def delete_row(self, row_index):
         '''
         '''
+        header_list = self.get_headers
+        for header in header_list:
+            self.delete_item(header, row_num)
 
-    def set_item(self, row_index, column_name, item):
+    def set_item(self, column_name, row_index, item):
         '''
         '''
+        # what if the row doesn't exist
+        # what if an item is already/not there?
+        pass
 
-    def get_item(self, row_index, column_name):
+    def get_item(self, column_name, row_index):
         '''
         '''
+        return self._data[column_name][row_index]
+
+    def delete_item(self, column_name, row_index):
+        '''
+        '''
+        del self._data[column_name][row_index]
 
 
 class Database():
