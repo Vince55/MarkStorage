0d516dd44484a636a433745904736c433f307dbf
Vince55 <vincent.nguyenv10@gmail.com>
2017-01-28 02:09:04 -0500

    Adding some input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     '''
     '''
+    introductions()
     # What I should do for now is put all the source code into one folder and
     # put main into another
 
@@ -16,6 +17,27 @@ def main():
     # Asks for command
     # Add, delete, calculate, close
 
+def introductions():
+    print("Welcome to Grade-a-base (still working on the name)")
+    print("Would you like open previous courses or add a new one?")
+    open_make_choice = input("Enter either open or make(open/make): ")
+    while (open_make_choice != "open" and open_make_choice != "make"):
+        print("Please enter either \"open\" or \"make\":")
+        open_make_choice = input()
+    if (open_make_choice == "make"):
+        make_new_table()
+    else:
+        open_existing_table()
 
-if (__name__ == "main"):
+
+def make_new_table():
+    '''
+    '''
+
+def open_existing_table():
+    '''
+    '''
+
+
+if (__name__ == "__main__"):
     main()
