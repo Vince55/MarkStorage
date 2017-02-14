a99c9fee299c5f64a076b15ae4d528eb5cda8d01
Vince55 <vincent.nguyenv10@gmail.com>
2017-02-13 20:05:39 -0500

    Modified input/output/interface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from file_read import *
 from file_write_append import *
+from user_input import *
 
 def main():
     '''
@@ -60,58 +61,6 @@ def introductions():
             modify_table()
         else:
             delete_table()
-
-def add_to_table():
-    '''
-    '''
-
-def modify_table():
-    '''
-    '''
-
-def delete_table():
-    '''
-    '''
-
-    # Need to remove table and re-write file
-
-
-def preview_all_tables(database):
-    '''
-    '''
-    # Make space beforehand
-    print()
-    print("----------")
-    for table in database.get_all_table_names():
-        print ("Table Name: " + table)
-        print()
-        database.get_table(table).print_table()
-        print()
-    print("Done!")
-    print("----------")
-    print()
-
-def make_new_table():
-    '''
-    '''
-
-def open_existing_table(database):
-    '''
-    '''
-    print()
-    list_of_tables = list(database.get_all_table_names())
-    print("These are the courses you currently have: ")
-    for table in list_of_tables:
-        print(table)
-
-    print()
-    print("Which course do you want to open?")
-    table_choice = input("Please enter the exact name as it is displayed above: ")
-    while (table_choice not in list_of_tables):
-        print("This course was not found in the database")
-        table_choice = input("Please try again: ")
-    database.get_table(table_choice).print_table()
-
 
 if (__name__ == "__main__"):
     main()
---
+++ user_input.py
@@ -0,0 +1,48 @@
+def add_to_table():
+    '''
+    '''
+    pass
+
+def modify_table():
+    '''
+    '''
+
+def delete_table():
+    '''
+    '''
+
+def preview_all_tables(database):
+    '''
+    '''
+    # Make space beforehand
+    print()
+    print("----------")
+    for table in database.get_all_table_names():
+        print ("Table Name: " + table)
+        print()
+        database.get_table(table).print_table()
+        print()
+    print("Done!")
+    print("----------")
+    print()
+
+def make_new_table():
+    '''
+    '''
+
+def open_existing_table(database):
+    '''
+    '''
+    print()
+    list_of_tables = list(database.get_all_table_names())
+    print("These are the courses you currently have: ")
+    for table in list_of_tables:
+        print(table)
+
+    print()
+    print("Which course do you want to open?")
+    table_choice = input("Please enter the exact name as it is displayed above: ")
+    while (table_choice not in list_of_tables):
+        print("This course was not found in the database")
+        table_choice = input("Please try again: ")
+    database.get_table(table_choice).print_table()
